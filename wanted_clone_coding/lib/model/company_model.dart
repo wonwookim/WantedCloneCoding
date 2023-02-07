1e3bb517c4b51267caf299024c9f0a81716b7e6c
wonwoo kim <kwo9911@naver.com>
2023-02-08 00:37:42 +0900

    <FEAT> model

---
+++ wanted_clone_coding/lib/model/company_model.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class company{
+  company({
+    required this.name,
+    required this.slogan,
+    required this.image,
+  });
+  String name;
+  String slogan;
+  String image;
+}
---
+++ wanted_clone_coding/lib/model/event_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Event{
+  Event({
+    required this.title,
+    required this.subtitle,
+    required this.image,
+  });
+
+  String title;
+  String subtitle;
+  String image;
+}
