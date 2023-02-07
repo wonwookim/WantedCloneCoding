d60e6ce4afcfa602489a075cd6e392871cc1bea2
wonwoo kim <kwo9911@naver.com>
2023-02-07 17:24:05 +0900

    <FIX> main.dart *theme darkTheme -> lightTheme

--- wanted_clone_coding/lib/main.dart
+++ wanted_clone_coding/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: MyTheme.darkTheme,
+      theme: MyTheme.lightTheme,
       home: BottomNavigationScreen(),
       debugShowCheckedModeBanner:false
     );
