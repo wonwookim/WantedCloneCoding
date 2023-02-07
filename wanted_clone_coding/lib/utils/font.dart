e0926ced6e9936073fd172813cb8a4c0b6352861
wonwoo kim <kwo9911@naver.com>
2023-02-07 17:22:49 +0900

    <FIX> font.dart *FontColor White -> Black

--- wanted_clone_coding/lib/utils/font.dart
+++ wanted_clone_coding/lib/utils/font.dart
@@ -5,82 +5,74 @@ class MyTextTheme {
   static TextTheme textTheme = const TextTheme(
     displayLarge: TextStyle(
       //Size22
-      fontSize:22,
+      fontSize: 22,
       fontWeight: FontWeight.w700,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     displayMedium: TextStyle(
       //Size18
-      fontSize:18,
+      fontSize: 18,
       fontWeight: FontWeight.w700,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     titleLarge: TextStyle(
       // title
       fontSize: 25,
       fontWeight: FontWeight.w500,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     titleMedium: TextStyle(
       // NavigationTitle
       fontSize: 18,
       fontWeight: FontWeight.w500,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     labelLarge: TextStyle(
       // mainbold
       fontSize: 14,
       fontWeight: FontWeight.w700,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     labelMedium: TextStyle(
       // main
       fontSize: 14,
       fontWeight: FontWeight.w400,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     bodyLarge: TextStyle(
       // mainboldheight
       fontSize: 14,
       fontWeight: FontWeight.w700,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     bodyMedium: TextStyle(
       // mainheight
       fontSize: 14,
       fontWeight: FontWeight.w400,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     bodySmall: TextStyle(
       // caption
       fontSize: 11,
       fontWeight: FontWeight.w400,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
     labelSmall: TextStyle(
       // tempfont
       fontSize: 11,
       fontWeight: FontWeight.w400,
       fontFamily: 'SUIT',
-      color: AppColors.mainWhite
     ),
   );
 
   static TextStyle size_22(BuildContext context) {
     return Theme.of(context).textTheme.displayLarge!;
   }
+
   static TextStyle size_18(BuildContext context) {
     return Theme.of(context).textTheme.displayMedium!;
   }
+
   static TextStyle title(BuildContext context) {
     return Theme.of(context).textTheme.titleLarge!;
   }
