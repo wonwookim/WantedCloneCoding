d768c6fc2d20943a654868ca65e142679406f299
wonwoo kim <kwo9911@naver.com>
2023-02-08 00:48:22 +0900

    <FIX> home_screen auto_change_widget

--- wanted_clone_coding/lib/screen/home/home_screen.dart
+++ wanted_clone_coding/lib/screen/home/home_screen.dart
@@ -1,20 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:wanted_clone_coding/controller/home_controller.dart';
 import 'package:wanted_clone_coding/utils/color.dart';
 import 'package:wanted_clone_coding/utils/font.dart';
 import 'package:wanted_clone_coding/widget/appbar_widget.dart';
+import 'package:wanted_clone_coding/widget/auto_change_widget.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
+  HomeScreen({Key? key}) : super(key: key);
+  HomeController controller = Get.put(HomeController());
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          AppbarWidget(),
-          Divider(height: 1,color: AppColors.maingray,)
-        ],
+    double statusBarHeight = MediaQuery.of(context).padding.top;
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        body: Column(
+          children: [
+            Stack(
+              children: [
+                change_color_area(),
+                Positioned(
+                    top: statusBarHeight + kToolbarHeight + 20,
+                    child: AutoChangeWidget(
+                      categoryText: 'EVENT',
+                      controller: controller.pController,
+                      eventList: controller.eventList,
+                      currentIndex: controller.currentIndex,
+                    ))
+              ],
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  Widget change_color_area() {
+    return Column(children: [
+      Obx(
+        () => Container(
+          decoration: BoxDecoration(
+              color: controller.colors.isNotEmpty
+                  ? controller.colors[controller.currentIndex.value].color
+                  : AppColors.mainblue),
+          height: 300,
+          child: Column(
+            children: [
+              AppbarWidget(),
+              Divider(
+                height: 1,
+                color: AppColors.maingray,
+              )
+            ],
+          ),
+        ),
+      ),
+      SizedBox(height: 100),
+    ]);
+  }
+
+  Widget fixed_color_area() {
+    return Container();
   }
 }
