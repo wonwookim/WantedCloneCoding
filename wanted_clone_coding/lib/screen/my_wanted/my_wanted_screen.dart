31102b5267532f14275522746b6a8ddc005635fb
gyuhee0413 <kimgyuhee0413@gmail.com>
2023-02-08 14:50:54 +0900

    git status fix

--- wanted_clone_coding/lib/screen/my_wanted/my_wanted_screen.dart
+++ wanted_clone_coding/lib/screen/my_wanted/my_wanted_screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:wanted_clone_coding/utils/color.dart';
 
 class MyWantedScreen extends StatelessWidget {
   const MyWantedScreen({Key? key}) : super(key: key);
@@ -6,7 +10,45 @@ class MyWantedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: Column(
+      children: [
+        // Container(
+        //   margin: EdgeInsets.all(100.0),
+        //   decoration:
+        //       BoxDecoration(color: Colors.orange, shape: BoxShape.circle),
+        // ),
+        CircleAvatar(
+          radius: 60.0,
+          backgroundColor: AppColors.maingray,
+        ),
+        Obx((() => CupertinoActionSheet(
+              actions: [
+                CupertinoActionSheetAction(
+                    onPressed: () {}, child: const Text('사진 선택')),
+                CupertinoActionSheetAction(
+                    onPressed: () {}, child: const Text('사진 찍기'))
+              ],
+              cancelButton: CupertinoActionSheetAction(
+                child: Text('취소'),
+                onPressed: () {},
+              ),
+            )))
+      ],
+    ));
   }
 }
+
+
+
+        // Obx((() => CupertinoActionSheet(
+        //       actions: [
+        //         CupertinoActionSheetAction(
+        //             onPressed: () {}, child: const Text('사진 선택')),
+        //         CupertinoActionSheetAction(
+        //             onPressed: () {}, child: const Text('사진 찍기'))
+        //       ],
+        //       cancelButton: CupertinoActionSheetAction(
+        //         child: Text('취소'),
+        //         onPressed: () {},
+        //       ),
+        //     )))
