4fbcf2c3a49347c171c2a10d3bd5b1610d86e82f
wonwoo kim <kwo9911@naver.com>
2023-02-09 15:23:14 +0900

    <FIX>dialog_widget.dart *update dialog_widget

--- wanted_clone_coding/lib/widget/dialog_widget.dart
+++ wanted_clone_coding/lib/widget/dialog_widget.dart
@@ -6,73 +6,66 @@ import 'package:wanted_clone_coding/utils/font.dart';
 void showOneButtonDialog({
   required String title,
   required String startContent,
-  String? highlightContent,
-  String? endContent,
   required Function() buttonFunction,
   required String buttonText,
-  Color? highlightColor,
-  Color? btnColor,
-  Color? btnTextColor,
 }) {
   Get.dialog(
     Material(
       color: Colors.transparent,
       child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 35),
+        padding: const EdgeInsets.symmetric(horizontal: 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
+              // padding: const EdgeInsets.symmetric(horizontal: 16),
               decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8),
+                  borderRadius: BorderRadius.circular(4),
                   color: AppColors.mainWhite),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    title,
-                    style: MyTextTheme.mainbold(Get.context!),
-                    textAlign: TextAlign.center,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  RichText(
-                    text: TextSpan(children: [
-                      TextSpan(
-                        text: startContent,
-                        style: MyTextTheme.mainheight(Get.context!),
+              child: Column(mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+                const SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  title,
+                  style: MyTextTheme.mainbold(Get.context!)
+                      .copyWith(fontWeight: FontWeight.w600),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  startContent,
+                  style: MyTextTheme.caption(Get.context!)
+                      .copyWith(color: AppColors.mainblack),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Divider(
+                  height: 1,
+                ),
+                GestureDetector(
+                  onTap: buttonFunction,
+                  child: Container(
+                    height: 45,
+                    decoration: const BoxDecoration(color: AppColors.mainWhite),
+                    child: Center(
+                      child: Text(
+                        '확인',
+                        style: MyTextTheme.buttonfont(Get.context!)
+                        .copyWith(color: AppColors.mainblue),
+                        textAlign: TextAlign.center,
                       ),
-                      TextSpan(
-                        text: highlightContent ?? "",
-                        style: MyTextTheme.mainheight(Get.context!).copyWith(
-                            color: highlightColor ?? AppColors.mainblue),
-                      ),
-                      TextSpan(
-                        text: endContent,
-                        style: MyTextTheme.mainheight(Get.context!),
-                      )
-                    ]),
-                    textAlign: TextAlign.center,
+                    ),
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  Row(
-                    children: [
-                      
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                ],
-              ),
+                )
+              ]),
             ),
           ],
         ),
