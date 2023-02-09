2dd44f7111e92877ce95b2ed58ffb61b2f803c13
wonwoo kim <kwo9911@naver.com>
2023-02-09 16:10:59 +0900

    <FIX> bottom_navigation_screen.dart *FIX bottom_navigation bar

--- wanted_clone_coding/lib/screen/bottom_navigation_screen.dart
+++ wanted_clone_coding/lib/screen/bottom_navigation_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wanted_clone_coding/controller/bottom_navigation_controller.dart';
@@ -23,6 +25,7 @@ class BottomNavigationScreen extends StatelessWidget {
       MyWantedScreen()
     ];
     return Scaffold(
+      extendBody: false,
       body: Obx(
         () => IndexedStack(
           index: controller.currentIndex.value,
@@ -31,31 +34,24 @@ class BottomNavigationScreen extends StatelessWidget {
       ),
       bottomNavigationBar: Obx(
         () => Container(
-          height: 56,
+          height: Platform.isAndroid ? 56 : 48,
           child: BottomNavigationBar(
-            backgroundColor: AppColors.popupGray,
-            elevation: 0,
-              showSelectedLabels: false,
-              showUnselectedLabels: false,
+              selectedFontSize: 0,
+              unselectedFontSize: 0,
               type: BottomNavigationBarType.fixed,
               currentIndex: controller.currentIndex.value,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              enableFeedback: false,
+              backgroundColor: AppColors.popupGray,
+              elevation: 0,
               onTap: controller.changeBottomNav,
               items: [
-                BottomNavigationBarItem(
-                    icon: Text('홈'),
-                    label: '홈'),
-                BottomNavigationBarItem(
-                    icon: Text('채용'),
-                    label: '채용'),
-                BottomNavigationBarItem(
-                    icon: Text('커뮤니티'),
-                    label: '커뮤니티'),
-                BottomNavigationBarItem(
-                    icon: Text('내 정보'),
-                    label: '내 정보'),
-                BottomNavigationBarItem(
-                    icon: Text('MY 원티드'),
-                    label: 'MY 원티드')
+                BottomNavigationBarItem(icon: Text('홈'), label: '홈'),
+                BottomNavigationBarItem(icon: Text('채용'), label: '채용'),
+                BottomNavigationBarItem(icon: Text('커뮤니티'), label: '커뮤니티'),
+                BottomNavigationBarItem(icon: Text('내 정보'), label: '내 정보'),
+                BottomNavigationBarItem(icon: Text('MY 원티드'), label: 'MY 원티드')
               ]),
         ),
       ),
