d460a6c74d490a14b2e32a9e1c18589bbedfd80d
wonwoo kim <kwo9911@naver.com>
2023-02-13 04:09:23 +0900

    <FEAT> home_screen.dart

--- wanted_clone_coding/lib/controller/home_controller.dart
+++ wanted_clone_coding/lib/controller/home_controller.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -9,6 +10,7 @@ import 'package:wanted_clone_coding/utils/color.dart';
 import 'package:wanted_clone_coding/utils/constant.dart';
 
 import '../model/event_model.dart';
+
 
 class HomeController extends GetxController {
   late PageController pController;
@@ -33,30 +35,35 @@ class HomeController extends GetxController {
 
   //----------key--------------------------
   GlobalKey gKey = GlobalKey();
+
+  //--------eventList---------------------
+  RxList<Event> eventList = <Event>[].obs;
+  RxList<Event> eventForDevelopList = <Event>[].obs;
   @override
   void refresh() {
     super.refresh();
     appbarActive(false);
   }
+
   @override
   void onInit() async {
- 
-    sController.addListener(() {  //Appbar 존재 유무 
-      if(sController.offset >= tag_y.value) {
+    await getTag().then((value) => getCareerInsight(activeTag.value));
+    await getEventForCareerDevelopList();
+    await getEventList();
+    await getPalettes();
+    screenState(ScreenState.success);
+    timerstart();
+    pController = PageController(
+        viewportFraction: 0.9, initialPage: eventList.length * 100);
+    sController.addListener(() {
+      //Appbar 존재 유무
+      if (sController.offset >= tag_y.value) {
         appbarActive(true);
-      }else{
+      } else {
         appbarActive(false);
         appBarTagIsTap(false);
       }
     });
-  
-    pController = PageController(
-        viewportFraction: 0.9, initialPage: eventList.length * 100);
-    await getTag().then((value) => getCareerInsight(activeTag.value));
-
-    await getPalettes();
-    screenState(ScreenState.success);
-    timerstart();
     pController.addListener(() {
       if (timer != null) {
         timer!.cancel();
@@ -125,23 +132,108 @@ class HomeController extends GetxController {
     return offset;
   }
 
+//------------------------------------Get event --------------------------------------------------------------------------------
+  Future<void> getEventList() async {
+    eventList.value = event_json.map((e) => Event.fromJson(e)).toList();
+  }
+//------------------------------------Get event for career develop ----------------------------------------------------
+
+  Future<void> getEventForCareerDevelopList() async {
+    eventForDevelopList.value =
+        event_for_career_develop_json.map((e) => Event.fromJson(e)).toList();
+  }
+
 // ------------------------------------DUMMY DATA --------------------------------------------------------------------
-  List<Event> eventList = [
-    Event(
-        title: '우리 회사를 소개합니다.',
-        subtitle: '회사에 대한 정보, 원티드가 찾아드릴게요',
-        image:
-            'https://loopusimage.s3.ap-northeast-2.amazonaws.com/post/image/20220823/2022-08-23T225601.553691_44_0_aspectRatio1.000.jpg'),
-    Event(
-        title: '2022 개발자 리포트',
-        subtitle: '지금 무료로 다운받으세요!',
-        image:
-            'https://loopusimage.s3.ap-northeast-2.amazonaws.com/profile_image/image_crop_3a8b995e-b61c-4b5a-926c-6c7f5366dd1f489681898.jpg'),
-    Event(
-        title: '원티드 매거진 & Workers',
-        subtitle: '무료 다운로드하고 경품도 받으세요!',
-        image:
-            'https://loopusimage.s3.ap-northeast-2.amazonaws.com/profile_image/image_cropper_1645676938168.jpg'),
+  List<Map<String, dynamic>> event_json = [
+    {
+      'title': '우리 회사를 소개합니다.',
+      'subtitle': '회사에 대한 정보, 원티드가 찾아드릴게요',
+      'start_date': null,
+      'end_date': null,
+      'tag': null,
+      'image':
+          'https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FbRQgj4%2Fbtq3n9auUMk%2Fx1jFjVPF3vwrv7dWJZmLok%2Fimg.png',
+    },
+    {
+      'title': '2022 개발자 리포트',
+      'subtitle': '지금 무료로 다운받으세요!',
+      'start_date': null,
+      'end_date': null,
+      'tag': null,
+      'image': 'https://static.wanted.co.kr/images/events/2680/11ae45d0.jpg'
+    },
+    {
+      'title': '원티드 매거진 & Workers',
+      'subtitle': '무료 다운로드하고 경품도 받으세요!',
+      'start_date': null,
+      'end_date': null,
+      'tag': null,
+      'image': 'https://static.wanted.co.kr/images/events/1797/dac1b4f4.jpg'
+    },
+  ];
+  List<Map<String, dynamic>> event_for_career_develop_json = [
+    {
+      'title': "스터디살롱: '직원경험' 개념 잡기",
+      'start_date': "20230111",
+      'end_date': "20230212",
+      'tag': [
+        {'tag_id': 10, 'tag': 'HR'},
+        {'tag_id': 11, 'tag': '회사생활'}
+      ],
+      'image': 'http://photo.sentv.co.kr/photo/2020/08/24/20200824100748.jpg',
+      'type': 'event'
+    },
+    {
+      'title': "인사담당자가 직접 말하는, 서류 통과가 잘 되는 이력서",
+      'tag': [
+        {'tag_id': 12, 'tag': '취업/이직'},
+      ],
+      'image':
+          'https://binaries.templates.cdn.office.net/support/templates/ko-kr/lt16412178_quantized.png',
+      'type': 'article'
+    },
+    {
+      'title': "새로운 시작, 전직 지원 프로그램",
+      'tag': [
+        {'tag_id': 12, 'tag': '취업/이직'},
+      ],
+      'image':
+          'http://openimage.interpark.com/goods_image_big/3/0/9/2/7806713092_l.jpg',
+      'type': 'promotion'
+    },
+    {
+      'title': "나다운 일의 시작, 원티드",
+      'tag': [
+        {'tag_id': 13, 'tag': '브랜딩'},
+        {'tag_id': 12, 'tag': '취업/이직'},
+        {'tag_id': 14, 'tag': '라이프스타일'},
+      ],
+      'image': 'https://static.wanted.co.kr/images/events/2255/f38b1a9d.jpg',
+      'type': 'campain'
+    },
+    {
+      'title': "합격하는 면접자의 비밀",
+      'tag': [
+        {'tag_id': 15, 'tag': '커리어고민'},
+        {'tag_id': 12, 'tag': '취업/이직'},
+        {'tag_id': 16, 'tag': '시리즈'},
+      ],
+      'image':
+          'https://image.aladin.co.kr/product/2803/20/letslook/8967990197_toc1.jpg',
+      'type': 'vod'
+    },
+    {
+      'title': "프리온보딩 프론트엔드 인턴십",
+      'tag': [
+        {'tag_id': 12, 'tag': '취업/이직'},
+        {'tag_id': 17, 'tag': '개발'},
+        {'tag_id': 18, 'tag': '교육'},
+      ],
+      'start_date': "20230125",
+      'end_date': "20230217",
+      'image': 'https://static.wanted.co.kr/images/events/2693/bb92c6fd.jpg',
+      'type': 'edu'
+    }
   ];
 
   List<Map<String, dynamic>> tag_json = [
