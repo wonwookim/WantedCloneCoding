ad56ead870100f371c006c32dbe6ceabfc17a530
wonwoo kim <kwo9911@naver.com>
2023-02-18 23:55:27 +0900

    <FIX> company_model *변수 추가

--- wanted_clone_coding/lib/model/company_model.dart
+++ wanted_clone_coding/lib/model/company_model.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Company{
+class Company {
   Company({
     required this.name,
     required this.slogan,
+    required this.field,
     required this.image,
+    required this.introImage
   });
   String name;
   String slogan;
+  String field;
   String image;
+  String introImage;
+
+  factory Company.fromJson(Map<String, dynamic> json) => Company(
+      name: json['name'],
+      slogan: json['slogan'] != null ? json['slogan'] : '',
+      field: json['field'],
+      image: json['image'] != null ? json['image'] : '',
+      introImage: json['intro_image'] != null ? json['intro_image'] : '',
+      );
 }
