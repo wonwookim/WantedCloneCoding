321c0bfdafda42b54a5d10581cb1bc281af3d06c
wonwoo kim <kwo9911@naver.com>
2023-02-23 16:18:45 +0900

    <FEAT> enum screen

--- wanted_clone_coding/lib/utils/constant.dart
+++ wanted_clone_coding/lib/utils/constant.dart
@@ -1,2 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
 enum ScreenState { normal, loading, disconnect, error, success }
-enum Screen{home, employment, community, myInfo, myWanted}
+
+enum Screen {
+  home(''),
+  employment(''),
+  community('커뮤니티'),
+  myInfo('내 정보'),
+  myWanted('My 원티드');
+
+  final String name;
+  const Screen(this.name);
+}
+
+List<Map<String, dynamic>> tag_json = [
+    {'tag_id': 0, 'tag': 'IT/기술'},
+    {'tag_id': 1, 'tag': '개발'},
+    {'tag_id': 2, 'tag': '커리어고민'},
+    {'tag_id': 3, 'tag': '취업/이직'},
+    {'tag_id': 4, 'tag': '데이터'},
+    {'tag_id': 5, 'tag': '회사생활'},
+    {'tag_id': 6, 'tag': 'HR'},
+    {'tag_id': 7, 'tag': '마케팅'}
+  ];
